221ab2703aa019fc3f9121f39a1476de2bc35c1e
RohanBera <berarohan88@gmail.com>
2023-04-23 00:30:27 -0400

    add post (fornt end)

--- frontend/genie/lib/screens/add_post_screen.dart
+++ frontend/genie/lib/screens/add_post_screen.dart
@@ -19,67 +19,169 @@ class _AddPostState extends State<AddPost> {
 
     return Scaffold(
       backgroundColor: mobileBackgroundColor,
-      appBar: AppBar(
-        backgroundColor: blueColor,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
-        title: const Text('Add Post'),
-        centerTitle: false,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Post',
-              style: TextStyle(
-                color: mobileBackgroundColor,
-                fontWeight: FontWeight.w300,
-                fontSize: 16,
-              ),
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
+      // appBar: AppBar(
+      //   backgroundColor: whiteColor,
+      //   leading: IconButton(
+      //     icon: const Icon(
+      //       Icons.arrow_back,
+      //       color: secondaryColor,
+      //     ),
+      //     onPressed: () {},
+      //   ),
+      //   title: const Text('Add Post'),
+      //   centerTitle: false,
+      //   actions: [
+      //     TextButton(
+      //       onPressed: () {},
+      //       child: const Text(
+      //         'Post',
+      //         style: TextStyle(
+      //           color: mobileBackgroundColor,
+      //           fontWeight: FontWeight.w300,
+      //           fontSize: 16,
+      //         ),
+      //       ),
+      //     )
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(16, 0, 22, 34.5),
+          child: Column(
             children: [
-              const CircleAvatar(
-                backgroundImage: AssetImage("assets/signup.jpeg"),
-              ),
               SizedBox(
-                width: MediaQuery.of(context).size.width * 0.45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Write a Caption...",
-                    border: InputBorder.none,
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Text('Discard'),
                   ),
-                  maxLines: 8,
-                ),
-              ),
-              SizedBox(
-                height: 45,
-                width: 45,
-                child: AspectRatio(
-                  aspectRatio: 487 / 451,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1682098155729-c0aff2dd10cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"),
-                        fit: BoxFit.fill,
-                        alignment: FractionalOffset.topCenter,
+                  Text(
+                    'CREATE',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey[300],
+                        letterSpacing: 2),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 13, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: redColor,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      child: const Text(
+                        "Post",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: whiteColor,
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 22,
+                    backgroundImage: AssetImage("assets/signup.jpeg"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Description of post/story?'),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      expands: true, // <-- SEE HERE
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: lightBlueColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: IconButton(
+                      color: whiteColor,
+                      padding: EdgeInsets.zero,
+                      // constraints: BoxConstraints(),
+                      onPressed: () {},
+                      icon: Icon(Icons.add),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: lightBlueColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          color: whiteColor,
+                          padding: EdgeInsets.zero,
+                          // constraints: BoxConstraints(),
+                          onPressed: () {},
+                          icon: Icon(Icons.image_outlined),
+                        ),
+                        IconButton(
+                          color: whiteColor,
+                          padding: EdgeInsets.zero,
+                          // constraints: BoxConstraints(),
+                          onPressed: () {},
+                          icon: Icon(Icons.gif_box_outlined),
+                        ),
+                        IconButton(
+                          color: whiteColor,
+                          padding: EdgeInsets.zero,
+                          // constraints: BoxConstraints(),
+                          onPressed: () {},
+                          icon: Icon(Icons.camera_alt_outlined),
+                        ),
+                        IconButton(
+                          color: whiteColor,
+                          padding: EdgeInsets.zero,
+                          // constraints: BoxConstraints(),
+                          onPressed: () {},
+                          icon: Icon(Icons.attach_file_outlined),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
--- frontend/genie/lib/utils/global_vars.dart
+++ frontend/genie/lib/utils/global_vars.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:genie/screens/add_post_screen.dart';
+// import 'package:genie/screens/old_discover_screen.dart';
 import 'package:genie/screens/discover_screen.dart';
+import 'package:genie/screens/spread_the_wishes.dart';
+import 'package:genie/screens/profile.dart';
 
- const webScreenSize = 600;
+const webScreenSize = 600;
 
- const homeScreenItems = [
-          DiscoverScreen(),
-          Text("rohan1"),
-          AddPost(),
-          Text("rohan3"),
-          Text("rohan4"),
-        ];
+const homeScreenItems = [
+  DiscoverScreen(),
+  Text("rohan1"),
+  AddPost(),
+  // DiscoverScreen(),
+  SpreadTheWishes(),
+  Profile(),
+];
